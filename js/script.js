650fd0e9757928893196247827fa8c3d6eb448ac
Obiora Amobi <obiora.amobi@gmail.com>
2016-02-26 11:38:23 +0000

    Complete Commit

--- js/script.js
+++ js/script.js
@@ -27,8 +27,8 @@ window.onload = function() {
 
 
 $('#newSearch').hide();
-
-//getting the nav bar to stick.
+$('#mapContainer').hide();
+//getting the search bar to stick to nav bar.
 
 $(document).ready(function() {
 
@@ -42,19 +42,20 @@ $(document).ready(function() {
       }
     });
 
-    //Fixing my map 
 
-    // var s = $("#mapContainer");
+    //fixing my map to page 
+
+    var s = $("#mapContainer");
     // var pos = s.position();                    
-    // $(window).scroll(function() {
-    //     var windowpos = $(window).scrollTop();
-    //     //s.html("Distance from top:" + pos.top + "<br />Scroll position: " + windowpos);
-    //     if (windowpos >= 698) {
-    //         s.addClass("stick");
-    //     } else {
-    //         s.removeClass("stick"); 
-    //     }
-    // });
+    $(window).scroll(function() {
+        var windowpos = $(window).scrollTop();
+        if (windowpos > $(window).height()*1.2) {
+            s.addClass("stick");
+        } else {
+            s.removeClass("stick"); 
+        }
+    });
+
 });
 
 // key authentication begin
@@ -86,6 +87,8 @@ parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
 // key authentication end
 
 $("#searchButton").on("click", function(e) {
+
+    $('#mapContainer').show("slow");
 
     e.preventDefault()
 
@@ -124,7 +127,6 @@ $("#searchButton").on("click", function(e) {
             var sBusinessesLocationLatitude = "";
             var sBusinessesLocationLongitude = "";
 
-            // Business Addresses
             var aBusinessAddress = [];
             var aMarkerAddresses = [];
 
@@ -140,12 +142,12 @@ $("#searchButton").on("click", function(e) {
 
                 var aBusinessAddress = oBusiness.location.address[0]; 
 
-                sOptions += "<div class='col-md-6'><div class='panel panel-default'><div class='panel-heading' style='background-color:black; color:white;'><h4>" 
-                + oBusiness.name + "</h4></div><div class='panel-body'><p>"+ "<div class='thumbnail' style='background-color:whitesmoke;'><img src="
-                +oBusiness.image_url+" class='img-rounded' style='width:150px;'></div>" + oBusiness.location.address[0] + 
+                sOptions += "<div class='col-md-6'><div class='panel panel-default' style='box-shadow: 0 0 1em 1em #ccc;'><div class='panel-heading' style='background-color:black; color:white;'><h3>" 
+                + oBusiness.name + "</h3></div><div class='panel-body'><p>"+ "<div class='thumbnail' style='background-color:whitesmoke;'><img src="
+                +oBusiness.image_url+" class='img-rounded' style='width:150px;'></div> <span class='glyphicon glyphicon-home' aria-hidden='true'></span>" + " " + oBusiness.location.address[0] + 
                 "<br><br> <p class='text-snippet'>" + oBusiness.snippet_text + 
                 "</p> <br> <span class='glyphicon glyphicon-phone-alt' aria-hidden='true'></span>" 
-                + oBusiness.display_phone + "<br><br>" + "<img src="+oBusiness.rating_img_url_large + "></p><a href='" 
+                + " " + oBusiness.display_phone + "<br><br>" + "<img src="+oBusiness.rating_img_url_large + "></p><a href='" 
                 + oBusiness.url + "' class='btn btn-default'>Find Out More..</a></div></div></div>";
 
                 sBusinessesLocationLatitude += "<li>" + sLatitude + "</li>";
@@ -153,7 +155,7 @@ $("#searchButton").on("click", function(e) {
 
                 //console.log (sLatitude + " - "+sLongitude);
 
-                // Business Addresses
+                // generating the google map with business address plots
                 
                 var map;
                 var elevator;
@@ -201,14 +203,5 @@ $("#searchButton").on("click", function(e) {
     }, "slow");
 
 });
-
-// hides "GO Again" button when clicked and shows "GO" button
-
-// $("#newSearch").on("click", function(e) {
-//     e.preventDefault()
-
-//     $('#searchButton').show(""); 
-
-// });
 
 
